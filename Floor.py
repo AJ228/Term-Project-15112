258944ab8a05f1bf9c4c793b90746865b6edc858
AJ228 <87118785+AJ228@users.noreply.github.com>
2021-11-15 20:04:39 +0300

    Updated floor design

--- Floor.py
+++ Floor.py
@@ -1,11 +1,11 @@
+# This file manages the movement of the floor as a sprite
 import pygame
 from GameObject import  GameObject
 
 class Floor(GameObject): # The player sprite(s)
     @staticmethod
     def init(screenWidth, screenHeight): # Loading the player sprite image
-        Floor.floorImage = pygame.transform.scale(pygame.transform.flip(
-            pygame.image.load('Floor.png').convert_alpha(),False,True),
+        Floor.floorImage = pygame.transform.scale(pygame.image.load('Game_Floor.png').convert_alpha(),
             (screenWidth, screenHeight/4))
 
     def __init__(self, x, y):
