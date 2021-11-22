21272adc28c755397dc36ac129ce9837bff0092d
AJ228 <87118785+AJ228@users.noreply.github.com>
2021-11-22 21:21:01 +0300

    Fixed jumping animations and added collision flags

--- Player.py
+++ Player.py
@@ -10,13 +10,15 @@ class Player(GameObject): # The player sprite(s) class is a subclass of GameObje
 
     def __init__(self, x, y):
         super(Player, self).__init__(x, y, Player.charImage)
-        self.vY = 7
+        self.vY = 8
         self.angle = 0
         self.startY = self.y # Stores height the character is at before jumping (currently static y value)
-        self.falling = True
-        self.turnAngle = 5 # Following attributes help with airborne movement
+        self.falling = False
+        self.turnAngle = 7 # Following attributes help with airborne movement
         self.jumpHeight = 0
         self.jumped = False
+        self.floorCollision = False # Flags to check collisions
+        self.obCollision = False
 
     def applyGravity(self): # Used when player sprite is falling
         self.startY += self.vY
@@ -26,9 +28,11 @@ class Player(GameObject): # The player sprite(s) class is a subclass of GameObje
     def update(self, keysDown, screenWidth, screenHeight):
         self.image = pygame.transform.rotate(self.baseImage, self.angle)
 
-        if keysDown(pygame.K_SPACE) and self.y == self.startY: # Allows jumping only when not in mid-air
+        if keysDown(pygame.K_SPACE) and self.y == self.startY and self.falling == False: # Allows jumping only when not in mid-air
             self.jumped = True
             self.falling = False
+            self.floorCollision = False
+            self.obCollision = False
 
         if self.jumped == True or self.falling == True:
             self.angle -= self.turnAngle # Makes the player sprite rotate when mid-air
@@ -36,23 +40,26 @@ class Player(GameObject): # The player sprite(s) class is a subclass of GameObje
         if self.jumped == True:
             self.jumpHeight += self.vY
 
-            if self.jumpHeight == 70: # There is only one type of jump so change height that way
-                self.jumped = False
-                self.falling = True  
+            if self.jumpHeight == 80: # There is only one type of jump so change height that way
+                self.jumped = False  
 
         if self.jumpHeight != 0 and self.jumped == False:
-                self.jumpHeight -= self.vY 
+            self.jumpHeight -= self.vY 
+
+        if (self.floorCollision == False and self.obCollision == False) and self.jumped == False:
+            # Makes sure the player falls when not touching the floor or an obstacle
+            self.falling = True
+        else:
+            self.falling = False
 
         if self.falling == True:
             self.applyGravity()  # Descending while midair
 
 
         if self.y == self.startY and self.angle % 90 != 0:
-            self.angle -= (self.angle % 90) # Makes sure player always lands on a flat surface
+            self.angle -= (self.angle % 90) # Makes sure player always lands with a flat surface
 
-        self.y = self.startY - self.jumpHeight # This is temporary until I implement collision detection
-                                               # between player and floor sprites
-        print(self.jumpHeight)
+        self.y = self.startY - self.jumpHeight 
 
         super(Player, self).update(screenWidth, screenHeight)
     
